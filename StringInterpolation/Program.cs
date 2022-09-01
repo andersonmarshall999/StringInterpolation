366fcaa959c4f63862e5bfb210f3b1914a9817b9
andersonmarshall999 <andersonmarshall999@gmail.com>
2022-08-31 21:26:53 -0500

    Initial commit

--- StringInterpolation/Program.cs
+++ StringInterpolation/Program.cs
@@ -6,7 +6,19 @@ namespace StringInterpolation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var date = DateTime.Now;
+            Console.WriteLine($"{date,40:MMMM dd, yyyy}");
+            Console.WriteLine($"{date:yyyy.MM.dd}");
+            Console.WriteLine($"Day {date:dd} of {date:MMMM, yyyy}");
+            Console.WriteLine($"Year: {date:yyyy}, Month: {date:MM}, Day: {date:dd}");
+            Console.WriteLine($"{date.DayOfWeek,10}");
+            Console.WriteLine($"{date,10:t}{date.DayOfWeek,10}");
+            Console.WriteLine($"h:{date:hh}, m:{date:mm}, s:{date:ss}");
+            Console.WriteLine($"{date:yyyy.MM.dd.hh.mm.ss}");
+            
+            var pi = Math.PI;
+            Console.WriteLine($"{pi:C}");
+            Console.WriteLine($"{pi,10:F3}");
         }
     }
 }
